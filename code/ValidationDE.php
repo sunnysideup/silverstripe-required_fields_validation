28b1875d61cb59b8f4cd834a1dbcbc9fd947dd78
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 13:51:54 +1300

    PATCH: module clean-up

--- code/ValidationDE.php
+++ code/ValidationDE.php
@@ -1,27 +1,27 @@
 <?php
 
-class ValidationDE extends DataExtension {
-	
-	public function validate(ValidationResult $validationResult) {
-		$requiredFields = Config::inst()->get($this->owner->class, 'required_fields', Config::INHERITED);
-		if($requiredFields) {
-			foreach($requiredFields as $name) {
-				$error = false;
-				if($this->owner->hasMethod($name)) {
-					$object = $this->owner->$name();
-					if(! $object->exists()) {
-						$error = true;
-					}
-				}
-				else if(! $this->owner->$name) {
-					$error = true;
-				}
-				if($error) {
-					$label = $this->owner->fieldLabel($name);
-					$validationResult->error(sprintf(_t('Form.FIELDISREQUIRED', '%s is required'), "\"$label\""), $name);
-				}
-			}
-		}
-		return $validationResult;
-	}
+class ValidationDE extends DataExtension
+{
+    public function validate(ValidationResult $validationResult)
+    {
+        $requiredFields = Config::inst()->get($this->owner->class, 'required_fields', Config::INHERITED);
+        if ($requiredFields) {
+            foreach ($requiredFields as $name) {
+                $error = false;
+                if ($this->owner->hasMethod($name)) {
+                    $object = $this->owner->$name();
+                    if (! $object->exists()) {
+                        $error = true;
+                    }
+                } elseif (! $this->owner->$name) {
+                    $error = true;
+                }
+                if ($error) {
+                    $label = $this->owner->fieldLabel($name);
+                    $validationResult->error(sprintf(_t('Form.FIELDISREQUIRED', '%s is required'), "\"$label\""), $name);
+                }
+            }
+        }
+        return $validationResult;
+    }
 }
